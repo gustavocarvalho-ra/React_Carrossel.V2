e90a072c884f18b7c156f001fc3d2008c01d4a54
GustavoCR021 <gustavoc1922@gmail.com>
2024-02-24 18:41:32 -0300

    at2

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+
+      <div className='logo'>
+        <img src='/static/images/super-shoes.png' alt='Super Shoes Logo'/>
+      </div>
+
+      <div className='carrosel'>
+        <div className='item'>
+          <div className='image'>
+            <img src='https://imgcentauro-a.akamaihd.net/230x230/94313731.jpg' alt='Shoe'/>
+          </div>
+
+          <div className='info'>
+            <span className='name'>Super Shoe 1</span>
+            <span className='oldPrice'>U$299.00</span>
+            <span className='price'>U$199.00</span>
+          </div>
+        </div>
+      </div>
+
     </div>
   );
 }
